6e5ffcfddbde76aefefcf4c08f6e43e6dd0d1308
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-11-02 22:27:37 +0100

    Added .gitignore. Removed deprecated instruction

--- daytime-gnome-theme.scala
+++ daytime-gnome-theme.scala
@@ -10,7 +10,8 @@ import java.util.Date
 import scala.sys.process._
 
 object Main extends App:
-  val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+  val calendar = Calendar.getInstance()
+  val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
   val backend = HttpClientSyncBackend()
   val response = basicRequest
   .get(uri"https://api.sunrise-sunset.org/json?lat=41.8933203&lng=12.4829321&date=today")
@@ -19,9 +20,11 @@ object Main extends App:
   val sunrise = (json \\ "sunrise").head
   val sunset = (json \\ "sunset").head
   val formatter: SimpleDateFormat = SimpleDateFormat("h:mm:ss a")
-  val sunriseTime: Date = formatter.parse(sunrise.as[String])
-  val sunsetTime: Date = formatter.parse(sunset.as[String])
-  if (sunriseTime.getHours() to sunsetTime.getHours()).contains(currentHour.toDouble) then
+  calendar.setTime(formatter.parse(sunrise.as[String]))
+  val sunriseTime: Int = calendar.get(Calendar.HOUR_OF_DAY)
+  calendar.setTime(formatter.parse(sunset.as[String]))
+  val sunsetTime: Int = calendar.get(Calendar.HOUR_OF_DAY)
+  if (sunriseTime to sunsetTime).contains(currentHour.toDouble) then
     "gsettings set org.gnome.desktop.interface color-scheme prefer-light".!!
   else
     "gsettings set org.gnome.desktop.interface color-scheme prefer-dark".!!
